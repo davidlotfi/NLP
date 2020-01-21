e4c587c370f31c25536dfdde32a73df6aa274c6b
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-21 23:42:59 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -2,4 +2,10 @@ from gensim.models import word2vec
 import pandas as pd
 
 #reading data
-Dataset =pd.read_csv('ic.csv',engine='python',na_values=['NA','?'])
+Dataset =pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
+
+# Molecules
+#print(Dataset.iloc[:,7])
+
+Molecule= Dataset.iloc[:,7]
+print(Molecule[7])
