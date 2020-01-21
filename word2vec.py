3c97494f0b64ec8034007b2294e9e29542705249
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-22 00:08:23 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -7,16 +7,16 @@ Dataset =pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
 # Molecules
 #print(Dataset.iloc[:,7])
 
-Molecule= Dataset.iloc[:,7]
+Molecule= Dataset.iloc[:,7].values
 #print(Molecule[7])
 
 #Build model vocabulary for molecules ( model of transormation word >>> vecteur )
 
-model = Word2Vec(size=10,min_count=1)
+model = Word2Vec(size=1,min_count=1)
 model.build_vocab(Molecule)
 
 total_exemples=model.corpus_count
 print(total_exemples)
 
-SmilesCanonical2 = model[model.wv.vocab]
-print(SmilesCanonical2)
+Molecule2vec= model[model.wv.vocab]
+print(Molecule2vec)
