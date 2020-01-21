90aadabb12e9ac84340953c9dfbba76d84257c4e
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-21 23:52:23 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -1,4 +1,4 @@
-from gensim.models import word2vec
+from gensim.models import Word2Vec
 import pandas as pd
 
 #reading data
@@ -8,4 +8,8 @@ Dataset =pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
 #print(Dataset.iloc[:,7])
 
 Molecule= Dataset.iloc[:,7]
-print(Molecule[7])
+#print(Molecule[7])
+
+#Build model vocabulary for molecules ( model of transormation word >>> vecteur )
+
+model = Word2Vec(size=10,min_count=1)
