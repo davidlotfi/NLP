26bdaabb69d1af94994ff7c4672c11f8808a5b75
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-24 23:15:24 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -4,21 +4,29 @@ import pandas as pd
 #reading data
 Dataset = pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
 
-# Molecules
-#print(Dataset.iloc[:,7])
-
+# Molecules colonne
 Molecule= Dataset.iloc[:,7].values
-print(Molecule.shape)
+
+#print(Molecule)
+#print(Molecule.shape)
 
 #Build model vocabulary for molecules ( model of transormation word >>> vecteur )
 
-model = Word2Vec(size=10,min_count=1)
+model = Word2Vec(size=1,min_count=1)
 model.build_vocab(Molecule)
 model.save("word2vec.model")
+
 total_exemples=model.corpus_count
-print(total_exemples)
 print(model)
+print(total_exemples)
 
 Molecule2vec= model[model.wv.vocab]
-print(Molecule2vec)
+#print(Molecule2vec)
 print(Molecule2vec.shape)
+model.train(Molecule2vec,total_examples=10, epochs=10)
+
+# en va imprimer la vecteur de chaque atom exp( N = ?)
+
+c = model.wv['c']
+
+print(c)
