e968735ea96d873d1a72ba83da44eaf82b159a1b
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-24 22:39:20 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -12,11 +12,12 @@ print(Molecule.shape)
 
 #Build model vocabulary for molecules ( model of transormation word >>> vecteur )
 
-model = Word2Vec(size=1,min_count=1)
+model = Word2Vec(size=10,min_count=1)
 model.build_vocab(Molecule)
 model.save("word2vec.model")
 total_exemples=model.corpus_count
 print(total_exemples)
+print(model)
 
 Molecule2vec= model[model.wv.vocab]
 print(Molecule2vec)
