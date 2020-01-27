02bb9a66b918bafcf5c85d741c2101d5ea722493
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-27 23:29:33 +0100

    Add files via upload

--- simple_model.py
+++ simple_model.py
@@ -12,3 +12,8 @@ total_examples = model.corpus_count
 print(model)
 print(wordV)
 print(total_examples)
+
+
+print('vector of a ',model.wv['a'])
+print('vector of b ',model.wv['b'])
+print('vector of d ',model.wv['d'])
