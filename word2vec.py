34bd6324c72cd63b76716289fb37d0cd37b8bcfa
Abdellatif boukernine <31359561+davidlotfi@users.noreply.github.com>
2020-01-27 23:52:17 +0100

    Add files via upload

--- word2vec.py
+++ word2vec.py
@@ -2,32 +2,29 @@ from gensim.models import Word2Vec
 import pandas as pd
 
 #reading data
-Dataset = pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
 
-# Molecules colonne
-Molecule= Dataset.iloc[:,7].values
+def dataManipulation():
+     Dataset = pd.read_csv('ic.csv',sep=';',engine='python',na_values=['NA','?'])
+     # Molecules colonne
+     Molecule= Dataset.iloc[:,7].values
+     return Molecule
 
-#print(Molecule)
+
 #print(Molecule.shape)
 
 #Build model vocabulary for molecules ( model of transormation word >>> vecteur )
+def modele():
+    model = Word2Vec(size=1,min_count=1)
+    model.build_vocab(dataManipulation())
+    model.save("word2vec.model")
+    return model
 
-model = Word2Vec(size=1,min_count=1)
-model.build_vocab(Molecule)
-model.save("word2vec.model")
 
-total_exemples=model.corpus_count
-print(model)
-print(total_exemples)
 
-Molecule2vec= model[model.wv.vocab]
-#print(Molecule2vec)
-print(Molecule2vec.shape)
-print(Molecule2vec)
-#model.train(Molecule2vec,total_examples=10, epochs=10)
 
-# en va imprimer la vecteur de chaque atom exp( N = ?)
+print(dataManipulation())
+print(modele())
 
-#c = model.wv['c']
-#print(c)
+
+
 
